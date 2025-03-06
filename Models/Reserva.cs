5d7524df60e88610c90afa710e69911be3882519
Roberto Emilio <robertoemilio1@hotmail.com>
2025-03-06 11:17:35 -0300

    Desafio Sistema Hospedagem de um Hotel em C# concluido - Robertoemilio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,43 +15,54 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            
+            try
+            {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            //IMPLEMENTADO!!! - Roberto - 05-03-2025
+            if (Suite.Capacidade >=hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
-            {
+            {              
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                //IMPLEMENTADO!!! - Roberto - 05-03-2025
+                throw new Exception ("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+            }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exceção Tratada: " + ex.Message);
             }
         }
 
         public void CadastrarSuite(Suite suite)
         {
             Suite = suite;
+            
         }
 
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            //IMPLEMENTADO!!! - Roberto - 05-03-2025            
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            //IMPLEMENTADO!!! - Roberto - 05-03-2025
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            //IMPLEMENTADO!!! - Roberto - 05-03-2025
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= (valor*10)/100;
             }
 
             return valor;
